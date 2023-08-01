b7048ecfce353ba11fa44bd6377fe9e9d34ecf9b
Ryan Bradley <ryanbradley315@gmail.com>
2023-08-01 16:40:26 -0500

    Refactor drag logic to utilize a copy of the selected component to ease in logic generation.

--- client/src/components/editableLink/EditableLink.tsx
+++ client/src/components/editableLink/EditableLink.tsx
@@ -1,4 +1,4 @@
-import { useContext, useRef, useState } from 'react'
+import { useContext, useRef } from 'react'
 import DragIcon from '../../assets/DragIcon'
 import LinkIcon from '../../assets/LinkIcon'
 import styles from './editableLink.module.css'
@@ -9,16 +9,22 @@ export default function EditableLink({
   index,
   id,
   linkUrl,
-  platform
+  platform,
+  initialTop,
+  startDrag,
+  isDragging,
+  copyRef
 }: {
-  index: number
+  index: number | null
   id: string
   linkUrl: string
   platform: string
+  initialTop?: string
+  startDrag: ((id: string, top: number) => void) | null
+  isDragging: boolean
+  copyRef?: React.RefObject<HTMLDivElement>
 }) {
   const { removeLink, updateLink } = useContext(DataContext)
-
-  const [isDragging, setIsDragging] = useState(false)
 
   const divRef = useRef<HTMLDivElement>(null)
 
@@ -38,182 +44,23 @@ export default function EditableLink({
     })
   }
 
-  const dragEventListener = (e: MouseEvent) => {
-    const mousePosition = e.clientY
-
-    if (divRef.current?.parentElement) {
-      const children = [...divRef.current.parentElement.children]
-
-      let nextChildIdx = index === children.length - 1 ? null : index + 1
-      let prevChildIdx = index === 0 ? null : index - 1
-
-      const findChild = (idx: number | null) => {
-        const child = idx != null ? children[idx] as HTMLDivElement : null
-        
-        return child
-      }
-
-      let nextChild = findChild(nextChildIdx)
-      let prevChild = findChild(prevChildIdx)
-
-      const { height } = divRef.current.getBoundingClientRect()
-
-      const setChildMargin = () => {
-        children.forEach((child, idx) => {
-          if (idx === nextChildIdx && prevChild) {
-            (child as HTMLDivElement).style.marginTop = height + 'px'
-          } else {
-            (child as HTMLDivElement).style.marginTop = ''
-          }
-        })
-
-        if (divRef.current?.parentElement) {
-          if (!nextChild) {
-            divRef.current.parentElement.style.paddingTop = ''
-            divRef.current.parentElement.style.paddingBottom = height + 'px'
-          } else if (!prevChild) {
-            divRef.current.parentElement.style.paddingTop = height + 'px'
-            divRef.current.parentElement.style.paddingBottom = ''
-          } else {
-            divRef.current.parentElement.style.paddingTop = ''
-            divRef.current.parentElement.style.paddingBottom = ''
-          }
-        }
-      }
-
-      setChildMargin()
-
-      const thresholds: {
-        up: null | number
-        down: null | number
-      } = {
-        up: null,
-        down: null
-      }
-
-      const calculateThresholds = () => {
-        thresholds.up = prevChild ? prevChild.getBoundingClientRect().top - (height / 4) : null
-        thresholds.down = nextChild ? nextChild.getBoundingClientRect().top + (height / 4) : null
-      }
-
-      calculateThresholds()
-
-      const topOffset = divRef.current.parentElement.getBoundingClientRect().top
-
-      const divPosition = mousePosition - topOffset - 30
-
-      const maxBottom 
-        = divRef.current.parentElement.getBoundingClientRect().height
-        - (height / 3)
-
-      divRef.current.style.top
-        = divPosition < (-1 * height / 3) ? (-1 * height / 3) + 'px'
-        : divPosition > maxBottom ? maxBottom + 'px'
-        : divPosition + 'px'
-
-      const targetTop = divRef.current.getBoundingClientRect().top
-
-      if (thresholds.up && targetTop < thresholds.up) {
-        if (!nextChildIdx) {
-          nextChildIdx = children.length - 1
-        } else {
-          nextChildIdx--
-          if (nextChildIdx === index) {
-            nextChildIdx--
-          }
-        }
-
-        if (prevChildIdx) {
-          prevChildIdx--
-
-          if (prevChildIdx === index) {
-            prevChildIdx--
-          }
-
-          if (prevChildIdx <= 0) {
-            prevChildIdx = null
-          }
-        }
-
-        nextChild = findChild(nextChildIdx)
-        prevChild = findChild(prevChildIdx)
-
-        setChildMargin()
-        calculateThresholds()
-      } 
-      
-      if (thresholds.down && targetTop > thresholds.down) {
-        if (!prevChildIdx) {
-          prevChildIdx = 0
-        } else {
-          prevChildIdx++
-          if (prevChildIdx === index) {
-            prevChildIdx++
-          }
-        }
-
-        if (nextChildIdx) {
-          nextChildIdx++
-          if (nextChildIdx === index) {
-            nextChildIdx++
-          }
-
-          if (nextChildIdx === children.length) {
-            nextChildIdx = null
-          }
-        }
-
-        nextChild = findChild(nextChildIdx)
-        prevChild = findChild(prevChildIdx)
-
-        setChildMargin()
-        calculateThresholds()
-      }
-    }
-  }
-
-  const startDrag = () => {
-    if (divRef.current) {
+  const handleMouseDown = () => {
+    if (divRef.current && startDrag) {
       const { top } = divRef.current.getBoundingClientRect()
 
-      const containerTop = divRef.current.parentElement?.getBoundingClientRect().top
-
-      setIsDragging(true)
-
-      divRef.current.style.top = top - (containerTop || 0) + 'px'
-
-      window.addEventListener('mousemove', dragEventListener)
-
-      window.addEventListener('mouseup', endDrag)
-    }
-  }
-
-  const endDrag = () => {
-    if (divRef.current) {
-      setIsDragging(false)
-
-      if (divRef.current.parentElement) {
-        const childWithMargin = [...divRef.current.parentElement.children].find(child => (
-          (child as HTMLDivElement).style.marginTop !== ''
-        )) as HTMLDivElement
-
-        if (childWithMargin) childWithMargin.style.marginTop = ''
-
-        divRef.current.parentElement.style.paddingTop = ''
-        divRef.current.parentElement.style.paddingBottom = ''
-      }
-
-      divRef.current.style.top = ''
-
-      window.removeEventListener('mousemove', dragEventListener)
+      startDrag(id, top)
     }
   }
 
   return (
     <div
       className={styles.link_wrapper}
-      ref={divRef}
-      data-drag={isDragging}
+      ref={copyRef || divRef}
+      data-copy={typeof copyRef != 'undefined'}
+      style={{
+        top: initialTop || '',
+        opacity: isDragging ? '0.4' : ''
+      }}
       id='id'
     >
 
@@ -221,7 +68,7 @@ export default function EditableLink({
 
         <button
           className={styles.drag_btn}
-          onMouseDown={startDrag}
+          onMouseDown={handleMouseDown}
         >
 
           <DragIcon />
@@ -229,7 +76,7 @@ export default function EditableLink({
         </button>
 
         <span>
-          Link #{index + 1}
+          Link #{(index || 0) + 1}
         </span>
 
         <button
--- client/src/routes/home/components/Links.tsx
+++ client/src/routes/home/components/Links.tsx
@@ -1,12 +1,75 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useRef, useState } from 'react'
 import Button from '../../../components/button/Button'
 import styles from '../home.module.css'
 import NoLinks from './NoLinks'
 import EditableLink from '../../../components/editableLink/EditableLink'
-import { DataContext } from '../../../contexts/DataContext'
+import { DataContext, Link } from '../../../contexts/DataContext'
 
 export default function Links() {
     const { links, addLink } = useContext(DataContext)
+
+    const [dragIdx, setDragIdx] = useState<null | number>(null)
+    const [targetLink, setTargetLink] = useState<null | Link>(null)
+
+    const linksRef = useRef<HTMLDivElement>(null)
+    const copyRef = useRef<HTMLDivElement>(null)
+
+    const dragEventListener = (e: MouseEvent) => {
+        const mousePosition = e.clientY
+    
+        if (linksRef.current && copyRef.current) {
+            const children = [...linksRef.current.children].filter(child => (
+                child.getAttribute('data-copy') !== 'true'
+            ))
+        
+            const { height } = copyRef.current.getBoundingClientRect()
+        
+            const { top } = linksRef.current.getBoundingClientRect()
+
+            copyRef.current.style.top = mousePosition - top - 30 + 'px'
+        }
+    }
+    
+    const startDrag = (id: string) => {
+        const targetLink = links.find(link => link.id === id)
+
+        if (!targetLink) return
+
+        const targetIdx = links.indexOf(targetLink)
+
+        setDragIdx(targetIdx)
+
+        setTargetLink(targetLink)
+
+        window.addEventListener('mousemove', dragEventListener)
+
+        window.addEventListener('mouseup', endDrag)
+    }
+
+    const endDrag = () => {
+        if (copyRef.current) {
+            if (copyRef.current.parentElement) {
+                copyRef.current.style.top = ''
+
+                setTargetLink(null)
+                setDragIdx(null)
+
+                window.removeEventListener('mousemove', dragEventListener)
+            }
+        }
+    }
+
+    useEffect(() => {
+        if (targetLink && copyRef.current && dragIdx != null) {
+            const { height } = copyRef.current.getBoundingClientRect()
+
+            const initialTop = (dragIdx * height) + 'px'
+
+            copyRef.current.style.top = initialTop
+
+            console.log(initialTop)
+        }
+    }, [targetLink, dragIdx])
 
     return (
         <section className={styles.section}>
@@ -32,6 +95,7 @@ export default function Links() {
 
             <div
                 className={styles.links}
+                ref={linksRef}
             >
 
                 {
@@ -40,6 +104,8 @@ export default function Links() {
                             <EditableLink
                                 key={link.id}
                                 index={idx}
+                                isDragging={dragIdx === idx}
+                                startDrag={startDrag}
                                 { ...link }
                             />
                         ))
@@ -47,6 +113,14 @@ export default function Links() {
                         <NoLinks />
                     )
                 }
+
+                {targetLink && <EditableLink
+                    index={dragIdx}
+                    copyRef={copyRef}
+                    isDragging={false}
+                    startDrag={null}
+                    { ...targetLink }
+                />}
 
             </div>
 
